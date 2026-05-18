6bc1f97c0ceb498388c87100ca748a7b8a393c3c
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-18 12:56:58 -0400

    Try

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     };
-    outputs = inputs@{ self, nixpkgs, ... }:
+    outputs = inputs@{ self, nixpkgs, config, pkgs, lib, ... }:
     let
         localHostName = "cyberservermkii";
     in {
         hostName = localHostName;
-        buildSystem = { config, pkgs, config, secrets, ... }: nixpkgs.lib.nixosSystem {
+        buildSystem = { secrets, ... }: nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [ 
                 (import ./configuration.nix  {
