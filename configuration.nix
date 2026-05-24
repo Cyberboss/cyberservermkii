d179f23660e7440418335e385e4fd015a48b1dbc
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-24 19:57:46 -0400

    Go

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,17 @@ let
         nix flake update --flake /etc/nixos
         nixos-rebuild switch
     '';
+    persist-directory = "/persist";
+    resonite-directory = "${persist-directory}/resonite-directory";
+
+    rml-stressless-headless = pkgs.fetchurl {
+        url = "https://codeberg.org/Raidriar/StresslessHeadless/releases/download/2.2.1/StresslessHeadless.dll";
+        sha256 = "0000000000000000000000000000000000000000000000000000";
+    };
+    rml-headless-tweaks = pkgs.fetchurl {
+        url = "https://github.com/New-Project-Final-Final-WIP/HeadlessTweaks/releases/download/v2.2.0/HeadlessTweaks.dll";
+        sha256 = "0000000000000000000000000000000000000000000000000000";
+    };
 in
 {
     boot.loader = {
@@ -30,10 +41,70 @@ in
 
     environment.systemPackages = [ update-script ];
 
-    services.openssh.enable = true;
+    services = {
+        openssh.enable = true;
+        resonite-headless = {
+            enable = true;
+            steam-username = secrets.steam-username;
+            steam-password = secrets.steam-password;
+            headless-code = secrets.headless-code;
+            enable-rml = true;
+            rml-mods = [
+                rml-stressless-headless
+                rml-headless-tweaks
+            ];
+            config-json = {
+                loginCredential = secrets.resonite-username;
+                loginPassword = secrets.resonite-password;
+                startWorlds = [
+                    {
+                        "$schema" = "https://raw.githubusercontent.com/Yellow-Dog-Man/JSONSchemas/main/schemas/HeadlessConfig.schema.json";
+                        sessionName = "Dominion's Flat";
+                        customSessionId = "U-1nPiX9NfQQ4:DominionsFlat";
+                        accessLevel = "ContactsPlus";
+                        description = "Dominion's personal hideaway. Come say hello!";
+                        hideFromPublicListing = false;
+                        tags = [
+                            "after"
+                            "glow"
+                            "cozy"
+                            "cyberpunk"
+                            "home" 
+                            "apartment" 
+                            "social" 
+                            "afterglow"
+                            "after glow"
+                            "dominion"
+                            "flat" 
+                            "memes" 
+                            "workshop"
+                        ];
+                        loadWorldUrl = "resrec:///U-1nPiX9NfQQ4/R-019e5c2f-eb38-71a2-9b80-021e121719c5";
+                        defaultUserRoles = {
+                            Dominion = "Admin";
+                        };
+                        autoInviteUsernames = [
+                            "Dominion"
+                        ];
+                        inviteRequestHandlerUsernames = [
+                            "Dominion"
+                        ];
+                        autoInviteMessage = "Astral connection re-established.";
+                        idleRestartInterval = 14400;
+                        saveOnExit = false;
+                        autoSleep = true;
+                        enableResoniteLink = false;
+                    }
+                ];
+                dataFolder = "${resonite-directory}/data";
+                cacheFolder = "${resonite-directory}/cache";
+                logsFolder = "${resonite-directory}/logs";
+            };
+        };
+    };
     
     nixpkgs.overlays = [ (final: prev: {
-        inherit (prev.lixPackageSets.stable)
+        inherit (prev.lixPackageSets.stable)~
         nixpkgs-review
         nix-eval-jobs
         nix-fast-build
--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        resonite-headless.url = "github:Cyberboss/resonite-headless-nix";
     };
     outputs = inputs@{ self, nixpkgs, ... }:
     let
