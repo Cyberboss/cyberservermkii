f13a1d5b999ef112ae50af61060fad2d7bc24a30
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-23 20:12:47 -0400

    Not supported

--- configuration.nix
+++ configuration.nix
@@ -21,10 +21,8 @@
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNL86w85bS/+5aDj8fe4gZ2obLiiRn+1lXhWA2tX7Jt eddsa-key-20241023"
         ];
     };
-    
-    services.openssh.enable = true;
 
-    system.copySystemConfiguration = true;
+    services.openssh.enable = true;
     
     nixpkgs.overlays = [ (final: prev: {
         inherit (prev.lixPackageSets.stable)
