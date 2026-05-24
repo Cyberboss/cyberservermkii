a173cd80697ddde428c3017ff322d8ca4a0ebfb0
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-23 20:10:51 -0400

    Nearly there

--- configuration.nix
+++ configuration.nix
@@ -21,16 +21,7 @@
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNL86w85bS/+5aDj8fe4gZ2obLiiRn+1lXhWA2tX7Jt eddsa-key-20241023"
         ];
     };
-
-    home-manager.users.dominion = {
-        programs.bash = {
-            enable = true;
-            enableCompletion = true;
-
-            home.stateVersion = config.system.stateVersion;
-        };
-    };
-
+    
     services.openssh.enable = true;
 
     system.copySystemConfiguration = true;
