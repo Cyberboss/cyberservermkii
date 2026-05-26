8c0657fb3fff8c7ab828e5f9384e53ea1f798b19
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-26 14:45:49 -0400

    Disable root

--- configuration.nix
+++ configuration.nix
@@ -32,12 +32,15 @@ in
 
     i18n.defaultLocale = "en_CA.UTF-8";
 
-    users.users.dominion = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-        openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNL86w85bS/+5aDj8fe4gZ2obLiiRn+1lXhWA2tX7Jt eddsa-key-20241023"
-        ];
+    users.users = {
+        dominion = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+            openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNL86w85bS/+5aDj8fe4gZ2obLiiRn+1lXhWA2tX7Jt eddsa-key-20241023"
+            ];
+        };
+        root.hashedPassword = "!";
     };
 
     environment.systemPackages = [ update-script ];
