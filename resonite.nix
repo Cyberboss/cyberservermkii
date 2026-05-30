a6ada001ae5d2d4cf84436ae62ee9a2478661905
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-30 00:14:29 -0400

    Birthday over

--- resonite.nix
+++ resonite.nix
@@ -70,7 +70,7 @@ in
             startWorlds = [
                 {
                     "$schema" = "https://raw.githubusercontent.com/Yellow-Dog-Man/JSONSchemas/main/schemas/HeadlessConfig.schema.json";
-                    sessionName = "<color=#>Dominion</color>'s Flat (<color=#FF0000>Bi</color><color=#FFA500>rt</color><color=#FFFF00>hd</color><color=#008000>ay</color> <color=#0000FF>Ba</color><color=#EE82EE>sh</color>!)";
+                    sessionName = "<color=#>Dominion</color>'s Flat";
                     customSessionId = "U-1nPiX9NfQQ4:DominionsFlat";
                     accessLevel = "ContactsPlus";
                     description = "Dominion's personal hideaway. Come say hello!";
