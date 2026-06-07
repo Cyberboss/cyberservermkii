5418c3e60eff5435e5c8dbb9179906766a2acef6
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-06-07 16:50:43 -0400

    Fix jellyfin?

--- jellyfin.nix
+++ jellyfin.nix
@@ -1,6 +1,24 @@
+{...}:
+let
+    service-name = "jellyfin";
+    home-directory = "/home/${service-name}/data";
+in
 {
-    services.jellyfin = {
+    services.${service-name} = {
         enable = true;
         openFirewall = true;
+        user = service-name;
+        group = service-name;
+        dataDir = home-directory;
+    };
+    
+    users = {
+      groups."${service-name}" = { };
+      users."${service-name}" = {
+        isSystemUser = true;
+        createHome = true;
+        group = service-name;
+        home = service-name;
+      };
     };
 }
