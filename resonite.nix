15704ab1adf8ec690ace352f134c6fccf7454d4c
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-06-17 17:16:59 -0400

    Admin not necessary

--- resonite.nix
+++ resonite.nix
@@ -51,9 +51,9 @@ let
             DiscordLinkToSession = true;
             PermissionLevels = {
                 U-1jLFy9ehNjs = "Owner";
-                U-The-Honeybee = "Administrator";
-                U-Charizmare = "Administrator";
-                U-hartofstone = "Administrator";
+                U-The-Honeybee = "Moderator";
+                U-Charizmare = "Moderator";
+                U-hartofstone = "Moderator";
             };
             DisableInteractivePrompt = true;
         };
