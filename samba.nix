39ed1d01689ca5ac6d2f9cebac1e5bff1685f439
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-07 18:58:34 -0400

    Fix subnet for samba?

--- samba.nix
+++ samba.nix
@@ -9,7 +9,7 @@
                     "server string" = config.networking.hostName;
                     "netbios name" = config.networking.hostName;
                     security = "user";
-                    "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+                    "hosts allow" = "192.168.2. 127.0.0.1 localhost";
                     "hosts deny" = "0.0.0.0/0";
                     "guest account" = "nobody";
                     "map to guest" = "bad user";
