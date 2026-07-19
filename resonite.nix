448f461fdc245dae19d20cdf44964594c07df6db
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-19 14:35:27 -0400

    Fix

--- resonite.nix
+++ resonite.nix
@@ -109,7 +109,7 @@ in
             shutdown-seconds = 600;
         };
         resonite-headless = {
-            skip-steam = true; # TEMP!!!
+            use-steam = false; # TEMP!!!
             depotdownloader-env-file = secrets.depotdownloader.path;
             enable-rml = true;
             disable-ready-notify = true;
