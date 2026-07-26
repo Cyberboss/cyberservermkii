4eb5ee3524799f6d0e93eff355e4934666a1c455
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-26 13:27:44 -0400

    Experimental postgres module
    Closes #5

---
+++ system/modules/postgres.nix
@@ -0,0 +1,35 @@
+{ lib, config, ... }:
+let backup-location = config.services.postgresqlBackup.location;
+in {
+  services = {
+    postgresql = {
+      enable = true;
+      enableTCPIP = true;
+      enusureUsers = [{
+        name = "superuser";
+        ensureDBOwnership = true;
+      }];
+
+    };
+    postgresqlBackup.enable = true;
+  };
+
+  systemd.services.postgresqlBackup.startAt = lib.mkForce [ ];
+
+  backups.postgresql = {
+    pre = lib.getExe (pkgs.writeShellScriptBin "backup-postgres.sh" ''
+      set -euxo pipefail
+
+      echo "Creating Postgres backup..."
+      systemctl start postgresqlBackup
+    '');
+    paths = [ backup-location ];
+    post = lib.getExe (pkgs.writeShellScriptBin "delete-postgres-backup.sh" ''
+      set -euxo pipefail
+      echo "Removing Postgres backups..."
+
+      shopt -s dotglob
+      rm -rf ${config.services.postgresqlBackup.location}/*
+    '');
+  };
+}
