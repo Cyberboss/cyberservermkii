63b33a0c745951b14044ef572b553893bdc041c8
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-28 16:35:47 -0400

    Setup gitea-mirror, pin sops-nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
     resonite-headless.url = "github:Cyberboss/resonite-headless-nix";
     resonite-dominion.url = "github:Cyberboss/resonite-dominion";
     sops-nix = {
-      url = "github:Mic92/sops-nix";
+      url = "github:Mic92/sops-nix/f1406619a3884cd5c47992a70b8b35c9c0fcb4c9";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    gitea-mirror = {
+      url = "github:RayLabsHQ/gitea-mirror/v3.21.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- system/gitea.nix
+++ system/gitea.nix
@@ -1,11 +1,14 @@
-{ config, pkgs, globals, lib, ... }:
+{ config, pkgs, globals, lib, inputs, ... }:
 let
   domain = "git.${globals.tld}";
+  mirror-domain = "git-mirror.${globals.tld}";
   secrets = config.secrets.gitea;
   backup-location = config.services.gitea.dump.backupDir;
   local-service-url = "http://localhost:${
       toString config.services.gitea.settings.server.HTTP_PORT
     }";
+  local-mirror-url =
+    "http://localhost:${toString config.services.gitea-mirror.port}";
 in {
   imports = [
     ./modules/backups.nix
@@ -13,21 +16,31 @@ in {
     ./modules/docker.nix
     ./modules/postgres.nix
     ./modules/secrets
+
+    inputs.gitea-mirror.nixosModules.default
   ];
 
   services = {
-    cloudflared.tunnels.primary-tunnel.ingress.${domain} = local-service-url;
+    cloudflared.tunnels.primary-tunnel.ingress = {
+      "${domain}" = local-service-url;
+      "${mirror-domain}" = local-mirror-url;
+    };
+
     gitea = {
       enable = true;
+
       database = {
         createDatabase = true;
         type = "postgres";
       };
+
       dump = {
         enable = true;
         file = "gitea.dmp --skip-db --skip-log"; # psycho argument injection
       };
+
       lfs.enable = true;
+
       settings = {
         log.ROOT_PATH = "/var/log/gitea";
         service.DISABLE_REGISTRATION = true;
@@ -38,12 +51,24 @@ in {
         };
       };
     };
+
     gitea-actions-runner.instances.local = {
       enable = true;
       name = "Local";
       tokenFile = secrets.runnerEnvironment.path;
       url = local-service-url;
       labels = [ "ubuntu-latest:docker://node:18-bullseye" ];
+    };
+
+    gitea-mirror = {
+      enable = true;
+      openFirewall = true;
+
+      # Perfect chronological order
+      mirrorIssueConcurrency = 1;
+      mirrorIssueConcurrency = 1;
+
+      environmentFile = secrets.mirrorEnvironment.path;
     };
   };
 
@@ -52,20 +77,37 @@ in {
     timers.gitea-dump.wantedBy = lib.mkForce [ ];
   };
 
-  backups.gitea = {
-    pre = lib.getExe (pkgs.writeShellScriptBin "backup-gitea.sh" ''
-      set -euxo pipefail
+  backups = {
+    gitea = {
+      pre = lib.getExe (pkgs.writeShellScriptBin "backup-gitea.sh" ''
+        set -euxo pipefail
 
-      echo "Creating Gitea backup..."
-      systemctl start gitea-dump
-    '');
-    paths = [ backup-location ];
-    post = lib.getExe (pkgs.writeShellScriptBin "delete-gitea-backup.sh" ''
-      set -euxo pipefail
-      echo "Removing Gitea backups..."
+        echo "Creating Gitea backup..."
+        systemctl start gitea-dump
+      '');
+      paths = [ backup-location ];
+      post = lib.getExe (pkgs.writeShellScriptBin "delete-gitea-backup.sh" ''
+        set -euxo pipefail
+        echo "Removing Gitea backups..."
 
-      shopt -s dotglob
-      rm -rf ${backup-location}/*
-    '');
+        shopt -s dotglob
+        rm -rf ${backup-location}/*
+      '');
+    };
+    gitea-mirror = {
+      pre = lib.getExe (pkgs.writeShellScriptBin "stop-gitea-mirror.sh" ''
+        set -euxo pipefail
+
+        echo "Stopping Gitea Mirror..."
+        systemctl stop gitea-mirror
+      '');
+      paths = [ config.services.gitea-mirror.dataDir ];
+      post = lib.getExe (pkgs.writeShellScriptBin "start-gitea-mirror.sh" ''
+        set -euxo pipefail
+
+        echo "Starting Gitea Mirror..."
+        systemctl start gitea-mirror
+      '');
+    };
   };
 }
