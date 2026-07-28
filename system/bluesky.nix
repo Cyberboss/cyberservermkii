d23469bb7ed9263cf34e9610420642f9517cd55d
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-28 16:31:34 -0400

    Safer bsky backups

--- system/bluesky.nix
+++ system/bluesky.nix
@@ -1,6 +1,6 @@
 { config, globals, lib, ... }:
 let
-  service-port = "3000";
+  service-port = 3000;
   domain = "bsky.${globals.tld}";
   pds-domain = "bsky-pds.${globals.tld}";
   secrets = config.secrets.bluesky;
@@ -13,15 +13,31 @@ in {
       goat.enable = true;
       pdsadmin.enable = true;
       environmentFiles = [ secrets.environment.path ];
-      settings.PDS_HOSTNAME = pds-domain;
+      settings = {
+        PDS_HOSTNAME = pds-domain;
+        PORT = service-port;
+      };
     };
 
     cloudflared.tunnels.primary-tunnel.ingress.${pds-domain} =
-      "http://localhost:${service-port}";
+      "http://localhost:${toString service-port}";
   };
 
   systemd.services.bluesky-pds.restartTriggers = secrets.restartTriggers;
 
-  backups.bluesky.paths =
-    [ config.services.bluesky-pds.settings.PDS_DATA_DIRECTORY ];
+  backups.bluesky = {
+    pre = lib.getExe (pkgs.writeShellScriptBin "stop-bluesky-pds.sh" ''
+      set -euxo pipefail
+
+      echo "Stopping Bluesky PDS..."
+      systemctl stop bluesky-pds
+    '');
+    paths = [ config.services.bluesky-pds.settings.PDS_DATA_DIRECTORY ];
+    post = lib.getExe (pkgs.writeShellScriptBin "start-bluesky-pds.sh" ''
+      set -euxo pipefail
+
+      echo "Starting Bluesky PDS..."
+      systemctl start bluesky-pds
+    '');
+  };
 }
