a9b5afd673c8b791cf520df983fe4ab9e89ab0b9
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-31 14:59:56 -0400

    No

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     gitea-mirror = {
       url =
         "github:Cyberboss/gitea-mirror/DominionFixes"; # Unpin me after https://github.com/RayLabsHQ/gitea-mirror/pull/350 is merged
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = inputs@{ self, nixpkgs, ... }:
