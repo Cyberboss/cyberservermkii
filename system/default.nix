0f0be4e8efd50c128142dd67c11a1615e5f04c34
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-31 12:24:25 -0400

    Brother

--- system/default.nix
+++ system/default.nix
@@ -72,7 +72,7 @@ in {
     };
     fail2ban = {
       enable = true;
-      maxRetrys = 5;
+      maxRetry = 5;
       bantime-increment.enable = true;
       ignoreIP = [ "192.168.0.0/16" ];
     };
