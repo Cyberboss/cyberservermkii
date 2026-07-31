26b9032f1b294de5d71ac6d78c453b8e7fe69ab7
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-07-31 18:46:57 -0400

    One definition rule

--- system/modules/postgres.nix
+++ system/modules/postgres.nix
@@ -1,5 +1,14 @@
 { lib, pkgs, config, ... }:
-let backup-location = config.services.postgresqlBackup.location;
+let
+  backup-location = config.services.postgresqlBackup.location;
+  delete-postgres-backups = lib.getExe
+    (pkgs.writeShellScriptBin "delete-postgres-backup.sh" ''
+      set -euxo pipefail
+      echo "Removing Postgres backups..."
+
+      shopt -s dotglob
+      rm -rf ${config.services.postgresqlBackup.location}/*
+    '');
 in {
   imports = [ ./backups.nix ];
 
@@ -20,16 +29,12 @@ in {
     pre = lib.getExe (pkgs.writeShellScriptBin "backup-postgres.sh" ''
       set -euxo pipefail
 
+      ${delete-postgres-backups}
+
       echo "Creating Postgres backup..."
       systemctl start postgresqlBackup
     '');
     paths = [ backup-location ];
-    post = lib.getExe (pkgs.writeShellScriptBin "delete-postgres-backup.sh" ''
-      set -euxo pipefail
-      echo "Removing Postgres backups..."
-
-      shopt -s dotglob
-      rm -rf ${config.services.postgresqlBackup.location}/*
-    '');
+    post = delete-postgres-backups;
   };
 }
