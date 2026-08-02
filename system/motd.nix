b1c2ea0eea404c2625e1aa4e73b8d85e87f3cabe
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-08-02 19:26:30 -0400

    Cleanup

--- system/motd.nix
+++ system/motd.nix
@@ -1,13 +1,12 @@
 { lib, pkgs, ... }: {
   environment.loginShellInit = ''
-    
-        echo -e "$\{CYAN}=== System Login Info ===$\{NC}"
-        echo "Host:      $HOSTNAME"
-        echo "Kernel:    $(uname -r)"
-        echo "Uptime:    $(uptime -p)"
-        echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
-        echo "=== === === === === ==="
-        echo ""
-        ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay}
+    echo "== System Login Info =="
+    echo "Host:      $HOSTNAME"
+    echo "Kernel:    $(uname -r)"
+    echo "Uptime:    $(uptime -p)"
+    echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
+    echo "=== === === === === ==="
+    echo ""
+    ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay} -r
   '';
 }
