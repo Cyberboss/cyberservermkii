bbbaed425a0ae3f1c2fc4842ce0dde1900272833
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-08-02 19:21:12 -0400

    Fix

--- system/motd.nix
+++ system/motd.nix
@@ -1,15 +1,14 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   environment.loginShellInit = ''
+    set -euxo pipefail
     
-        set -euxo pipefail
-    
-        echo -e "$\{CYAN}=== System Login Info ===$\{NC}"
-        echo "Host:      $HOSTNAME"
-        echo "Kernel:    $(uname -r)"
-        echo "Uptime:    $(uptime -p)"
-        echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
-        echo "=== === === === === ==="
-        echo ""
-        ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay}
+    echo -e "$\{CYAN}=== System Login Info ===$\{NC}"
+    echo "Host:      $HOSTNAME"
+    echo "Kernel:    $(uname -r)"
+    echo "Uptime:    $(uptime -p)"
+    echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
+    echo "=== === === === === ==="
+    echo ""
+    ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay}
   '';
 }
