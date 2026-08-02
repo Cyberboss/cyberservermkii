629d45ed6ecf962cf840093e380b974998195646
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-08-02 19:23:42 -0400

    Bad idea

--- system/motd.nix
+++ system/motd.nix
@@ -1,14 +1,13 @@
 { lib, pkgs, ... }: {
   environment.loginShellInit = ''
-    set -euxo pipefail
     
-    echo -e "$\{CYAN}=== System Login Info ===$\{NC}"
-    echo "Host:      $HOSTNAME"
-    echo "Kernel:    $(uname -r)"
-    echo "Uptime:    $(uptime -p)"
-    echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
-    echo "=== === === === === ==="
-    echo ""
-    ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay}
+        echo -e "$\{CYAN}=== System Login Info ===$\{NC}"
+        echo "Host:      $HOSTNAME"
+        echo "Kernel:    $(uname -r)"
+        echo "Uptime:    $(uptime -p)"
+        echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
+        echo "=== === === === === ==="
+        echo ""
+        ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay}
   '';
 }
