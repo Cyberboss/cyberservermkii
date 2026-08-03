466bafac3ff87a6334db76b4cfde61b25325dfad
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-08-03 13:06:41 -0400

    Let me in!

--- system/users/dominion.nix
+++ system/users/dominion.nix
@@ -6,4 +6,9 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNL86w85bS/+5aDj8fe4gZ2obLiiRn+1lXhWA2tX7Jt eddsa-key-20241023"
     ];
   };
+
+  services.postgresql.ensureUsers = [{
+    name = "dominion";
+    superuser = true;
+  }];
 }
