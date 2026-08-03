c11156597835bc246f7ccd3fc83c44b77840354f
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-08-03 10:54:45 -0400

    Complain if the backups test failed on login

--- system/motd.nix
+++ system/motd.nix
@@ -1,12 +1,19 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, config, ... }: {
   environment.loginShellInit = ''
     echo "== System Login Info =="
     echo "Host:      $HOSTNAME"
     echo "Kernel:    $(uname -r)"
-    echo "Uptime:    $(uptime -p)"
+    echo "Uptime:    $(uptime)"
     echo "Memory:    $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
     echo "=== === === === === ==="
     echo ""
     ${lib.getExe pkgs.fortune} | ${lib.getExe pkgs.cowsay} -r
+    ${
+      if builtins.hasAttr "backups" config then ''
+        echo ""
+        echo -e "\e[1;31m!!!BACKUP PREPARATION TEST FAILED!!!\e[0m"
+      '' else
+        ""
+    }
   '';
 }
