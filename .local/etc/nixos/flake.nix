b856d772fe8900c70d4abefa80d0a88177c0b419
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-09-04 17:28:14 -0400

    Cleanup

--- .local/etc/nixos/flake.nix
+++ .local/etc/nixos/flake.nix
@@ -1,6 +1,6 @@
 # Exact contents
 {
-  inputs = { remote.url = "github:Cyberboss/cyberservermkii"; };
+  inputs.remote.url = "github:Cyberboss/cyberservermkii";
   outputs = inputs@{ self, remote, ... }:
     let hardware-configuration = ./hardware-configuration.nix;
     in { nixosConfigurations = (remote.build-system hardware-configuration); };
