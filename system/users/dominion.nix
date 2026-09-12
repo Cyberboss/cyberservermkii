3050b49486e2d6deb4498d71808cdca3aa74dc75
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-09-12 13:29:04 -0400

    Add github prefetch to my packages

--- system/users/dominion.nix
+++ system/users/dominion.nix
@@ -1,10 +1,14 @@
-{
+{ pkgs, ... } :{
   users.users.dominion = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" "jellyfin" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNL86w85bS/+5aDj8fe4gZ2obLiiRn+1lXhWA2tX7Jt eddsa-key-20241023"
     ];
+
+    packages = [
+      pkgs.nix-prefetch-github
+    ]
   };
 
   services.postgresql.ensureUsers = [{
